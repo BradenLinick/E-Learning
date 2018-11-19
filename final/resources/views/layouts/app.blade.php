12373f285ae6341b96cb44558f551bce8010cd7c
BradenLinick <bradylinick@Bradys-MacBook-Air.local>
2018-11-19 16:34:54 +0100

    Favicon added

--- final/resources/views/layouts/app.blade.php
+++ final/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-
+    <link rel="shortcut icon" type="image/png" href="/img/sjcnlogo.png">
     <!-- Styles -->
 
     <link href="{{ asset('css/app.min.css') }}" rel="stylesheet">
