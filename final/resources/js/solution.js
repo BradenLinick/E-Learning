a8c09582a34ca3ec1d5153bd518f674ce9690b7d
kybernetic <kybernetic@tuta.io>
2018-11-19 14:25:37 +0100

    solution class

--- final/resources/js/solution.js
+++ final/resources/js/solution.js
@@ -1,11 +1,26 @@
+import swal from 'sweetalert';
+
 class Solution {
     constructor(exerciseSolution) {
         this.exerciseSolution = exerciseSolution;
-        this.userSolution = userSolution;
-        this.str = str;
+    }
+
+    checkUserSolution(userSolution){
+        if(this.trimSpaces(this.exerciseSolution.toLowerCase()) === this.trimSpaces(userSolution.toLowerCase())) {
+            swal({
+                position: 'top-end',
+                icon: 'success',
+                title: 'You solved the exercise',
+                button: 'next'            
+              });
+        }
     }
 
     trimSpaces(str) {
         return str.replace(/\s+/g, '');
     }
+
+
 }
+
+//REMEMBER TO STOP THE TIMER WHEN A USER SOLVES A LESSON
