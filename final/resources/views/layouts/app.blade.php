6729db5787da2721927663816ba28ee27c59c325
kybernetic <kybernetic@tuta.io>
2018-11-21 16:09:18 +0100

    fixed logo

--- final/resources/views/layouts/app.blade.php
+++ final/resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <div id="app">
 
         <nav id="nav" class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="{{ url('/') }}">Super Jedi C<img src="/img/sjcnlogo.png" alt="logo">de Ninjas</a>
+            <a class="navbar-brand" href="{{ url('/') }}">Super Jedi C<img src="{{ asset('/img/sjcnlogo.png')}}" alt="logo">de Ninjas</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
