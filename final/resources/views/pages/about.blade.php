3bc9d9b426ed4a7a7ea8004b4b24cee007781901
kybernetic <kybernetic@tuta.io>
2018-11-23 11:12:47 +0100

    about pic

--- final/resources/views/pages/about.blade.php
+++ final/resources/views/pages/about.blade.php
@@ -14,7 +14,7 @@
   </div>
   <div id="div"></div>
   <div id="about" class="my-3 d-flex justify-content-between">
-    <img id="profile-bra" src="{{ asset('img/prophoto.jpg')}}" alt="">
+    <img id="profile-bra" src="{{ asset('img/Prophoto.jpg')}}" alt="">
     <div class="d-flex flex-column justify-content-around align-items-center">
       <h4 id="name" class="text-white">Braden Linick</h4>
       <p id="profile-text" class="text-white">Braden has travelled all over the world while working in the teaching industry long before getting a foot in the development industry. He's an honors university graduate and completed a coding bootcamp in Prague in 2018. His love for helping people learn and reach their career goals was a main driving force behind him co-founding SJCNs.</p>
